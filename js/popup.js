a052e29735677e0835c3cd2245092d27c98d9f90
AnastasiaArt <art-nastya@inbox.ru>
2018-07-08 23:12:50 +0300

    попап3

--- js/popup.js
+++ js/popup.js
@@ -4,42 +4,42 @@
   var departure = popup.querySelector("[name=data-out]");
   var isStorageSupport = true;
   var storage = "";
+  popup.classList.remove("modal-show");
   try {
     storage = localStorage.getItem("data-in");
     } catch (err) {
     isStorageSupport = false;
     }
-
-    link.addEventListener("click", function (evt) {
-      if (!popup.classList.contains("modal-show")){
-      evt.preventDefault();
-            popup.classList.add("modal-show");
-      if (storage) {
-      arrival.value = storage;
-    }
-    }else {
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
-    }
-      arrival.focus();
-    });
-    popup.addEventListener("submit", function (evt) {
-      if (!arrival.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-      console.log("Нужно ввести дату заезда");
-    } else {
-      localStorage.setItem("data-in", arrival.value);
-    }
-    });
-    window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
+      link.addEventListener("click", function (evt) {
+        if (!popup.classList.contains("modal-show")){
+        evt.preventDefault();
+              popup.classList.add("modal-show");
+        if (storage) {
+        arrival.value = storage;
+      }
+      }else {
         popup.classList.remove("modal-show");
         popup.classList.remove("modal-error");
       }
-    }
-    });
+        arrival.focus();
+      });
+      popup.addEventListener("submit", function (evt) {
+        if (!arrival.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+        console.log("Нужно ввести дату заезда");
+      } else {
+        localStorage.setItem("data-in", arrival.value);
+      }
+      });
+      window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+          popup.classList.remove("modal-show");
+          popup.classList.remove("modal-error");
+        }
+      }
+      });
