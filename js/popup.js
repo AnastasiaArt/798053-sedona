fba166c0c3321db0f6d9ec153ff447720f2be560
AnastasiaArt <art-nastya@inbox.ru>
2018-07-09 15:52:42 +0300

    готово на 2 проверку

--- js/popup.js
+++ js/popup.js
@@ -30,7 +30,6 @@
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
-      console.log("Нужно ввести дату заезда");
     } else {
       localStorage.setItem("data-in", arrival.value);
     }
